f6feab264caf227a200e92e22875316af6ef856e
developersajeeb <sajeebdeveloper@gmail.com>
2025-12-17 23:54:32 +0600

    add 2 new page ui

--- src/pages/user/my-profile/index.tsx
+++ src/pages/user/my-profile/index.tsx
@@ -1,29 +1,53 @@
 import NotUserIcon from "@/components/shared/blocks/NotUserIcon";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useUserInfoQuery } from "@/redux/features/auth/auth.api";
 import { TbEdit } from "react-icons/tb";
 import { Link } from "react-router";
 
 
 const MyProfile = () => {
+    const { data: userData, isLoading } = useUserInfoQuery(undefined);
+    
     return (
         <>
             <h2 className="text-xl font-bold text-primary-950 mb-4">My Profile <Link to='/user/settings' className="w-8 h-8 text-primary-500 hover:text-white rounded-full bg-primary-100 hover:bg-primary-500 duration-300 inline-flex justify-center items-center ml-2"><TbEdit size={18} /></Link></h2>
             <div className="p-5 border border-gray-300 rounded-xl">
                 <h3 className="text-lg font-semibold text-gray-700">Basic Information</h3>
-
                 <NotUserIcon className="rounded-xl my-5" />
+                <div className="flex flex-wrap gap-5 lg:gap-8">
+                    <div>
+                        <p className="font-bold text-gray-600 text-sm">Name</p>
+                        <p className="font-medium text-gray-800 text-base">{isLoading ? <Skeleton className="w-40 h-5 mt-1"/> : userData?.data?.name ?? 'N/A'}</p>
+                    </div>
+                    <div>
+                        <p className="font-bold text-gray-600 text-sm">Email</p>
+                        <p className="font-medium text-gray-800 text-base break-all">{isLoading ? <Skeleton className="w-40 h-5 mt-1"/> : userData?.data?.email ?? 'N/A'}</p>
+                    </div>
+                    <div>
+                        <p className="font-bold text-gray-600 text-sm">Phone</p>
+                        <p className="font-medium text-gray-800 text-base">{isLoading ? <Skeleton className="w-40 h-5 mt-1"/> : userData?.data?.phone ?? 'N/A'}</p>
+                    </div>
+                </div>
 
-                <div className="grid grid-cols-3 gap-5">
+                <div className="w-full h-[1px] bg-gray-300 my-5"></div>
+
+                <h3 className="text-lg font-semibold text-gray-700">Address Information</h3>
+                <div className="flex flex-wrap gap-5 lg:gap-8 mt-4">
                     <div>
-                        <p className="font-bold text-gray-600 text-sm">Name</p>
-                        <p className="font-medium text-gray-800 text-base">Sajeeb Debnath</p>
+                        <p className="font-bold text-gray-600 text-sm">Address</p>
+                        <p className="font-medium text-gray-800 text-base">4530 Clousson Road, Houston</p>
                     </div>
                     <div>
-                        <p className="font-bold text-gray-600 text-sm">Name</p>
-                        <p className="font-medium text-gray-800 text-base">Sajeeb Debnath</p>
+                        <p className="font-bold text-gray-600 text-sm">Country</p>
+                        <p className="font-medium text-gray-800 text-base break-all">United States Of America</p>
                     </div>
                     <div>
-                        <p className="font-bold text-gray-600 text-sm">Name</p>
-                        <p className="font-medium text-gray-800 text-base">Sajeeb Debnath</p>
+                        <p className="font-bold text-gray-600 text-sm">City</p>
+                        <p className="font-medium text-gray-800 text-base">San Francisco</p>
+                    </div>
+                    <div>
+                        <p className="font-bold text-gray-600 text-sm">Postal Code</p>
+                        <p className="font-medium text-gray-800 text-base">94105</p>
                     </div>
                 </div>
             </div>
--- src/pages/user/notifications/index.tsx
+++ src/pages/user/notifications/index.tsx
@@ -1,9 +1,35 @@
+import { IoNotificationsOutline } from "react-icons/io5";
 
 const Notifications = () => {
     return (
-        <div>
-            <h1>Notifications</h1>
-        </div>
+        <>
+            <h2 className="text-xl font-bold text-primary-950 mb-4">Notifications</h2>
+            <ul className="space-y-4">
+                <li className="flex gap-2 px-4 py-5 border border-gray-200 rounded-xl">
+                    <span className="text-[22px] text-gray-500 bg-gray-300 w-12 min-w-12 h-12 rounded-full inline-flex justify-center items-center"><IoNotificationsOutline /></span>
+                    <div>
+                        <p className="text-base text-gray-700 font-semibold">Booking Confirmation</p>
+                        <p className="text-sm">Thank you for choosing Air. Your adventure is set.</p>
+                    </div>
+                </li>
+                <li className="flex gap-2 px-4 py-4 rounded-xl bg-primary-50 border border-gray-200 cursor-pointer">
+                    <span className="text-[22px] text-primary-400 bg-primary-100 w-12 min-w-12 h-12 rounded-full inline-flex justify-center items-center"><IoNotificationsOutline /></span>
+                    <div className="flex-1">
+                        <p className="text-base font-semibold text-primary-700">Rescheduling Notification</p>
+                        <p className="text-sm text-primary-700">Thank you for choosing Air. Your adventure is set.</p>
+                    </div>
+                    <span className="bg-primary-400 hover:bg-primary-600 cursor-pointer duration-300 w-3 h-3 rounded-full"></span>
+                </li>
+                <li className="flex gap-2 px-4 py-4 rounded-xl bg-primary-50 border border-gray-200 cursor-pointer">
+                    <span className="text-[22px] text-primary-400 bg-primary-100 w-12 min-w-12 h-12 rounded-full inline-flex justify-center items-center"><IoNotificationsOutline /></span>
+                    <div className="flex-1">
+                        <p className="text-base font-semibold text-primary-700">Rescheduling Notification</p>
+                        <p className="text-sm text-primary-700">Thank you for choosing Air. Your adventure is set.</p>
+                    </div>
+                    <span className="bg-primary-400 hover:bg-primary-600 cursor-pointer duration-300 w-3 h-3 rounded-full"></span>
+                </li>
+            </ul>
+        </>
     );
 };
 
